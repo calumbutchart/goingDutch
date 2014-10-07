964749b9062a4d9b21449a06ac3514a1bc4b187f
mike skinner <mike@strawberrysoup.co.uk>
2014-10-07 19:00:15 +0100

    last minute!!!!

--- templates/header.php
+++ templates/header.php
@@ -6,7 +6,7 @@
 <head>
 <meta charset="UTF-8">
 <title></title>
-
+ <meta name="viewport" content="width=device-width, user-scalable=no">
 <script data-main="/js/main" src="/js/require.js"></script>
 <link rel="stylesheet" type="text/css" href="/css/main.css" />
 </head>
